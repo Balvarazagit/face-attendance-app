4cd95588246f71da992e8005247938e77ca819d8
Balvarazagit <211260116007setiit@gmail.com>
2026-04-10 16:18:19 -0400

    update full code

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: `${process.env.FRONTEND_URL}`,
+    credentials: true,
+  }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
